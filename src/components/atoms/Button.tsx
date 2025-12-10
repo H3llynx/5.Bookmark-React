9508e0b2e6e40d064f7706ccd68f2f0788de9157
Helene T <helene.teychene@gmail.com>
2025-12-10 12:43:41 +0100

    feat/created button component

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,19 @@
+interface ButtonProps {
+  text: string;
+  color: "red" | "blue";
+}
+
+export function Button({ text, color }: ButtonProps) {
+
+  const variants = {
+    red: "my-button  uppercase py-0.9 px-0.8 md:px-2 md:py-0.75 md:bg-red md:text-white md:hover:bg-white md:hover:text-red md:border-red",
+    blue: "my-button font-medium py-[0.9rem] px-[0.8rem] md:px-[1.6rem] bg-blue text-white border-blue md:hover:bg-white md:hover:text-blue"
+  }
+
+  return (
+    <button className={variants[color]}
+      tabIndex={0}>{text}
+    </button>
+  )
+}
+
