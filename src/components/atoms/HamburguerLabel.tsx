8a760b223c5c740b147eb591a68902f6af21481a
Helene T <helene.teychene@gmail.com>
2025-12-10 17:24:11 +0100

    feat/created the label componant for the mo bile devices hamburguer menu

--- src/components/atoms/HamburguerLabel.tsx
+++ src/components/atoms/HamburguerLabel.tsx
@@ -1,10 +1,12 @@
 import CloseIcon from "../../assets/images/icon-close.svg"
 import HamburgerIcon from "../../assets/images/icon-hamburger.svg"
+import LogoMenu from "../../assets/images/logo-bookmark-menu.svg"
 
 export function HamburguerLabel() {
 
     return (
-        <label htmlFor="menu-toggle" aria-label="open/close menu" className="relative">
+        <label htmlFor="menu-toggle" aria-label="open/close menu" className="relative flex justify-between">
+            <img src={LogoMenu} alt="bookmark logo" className="hidden group-has-checked:block" />
             <input type="checkbox" className="absolute z-2 opacity-0" id="menu-toggle" />
             <img src={HamburgerIcon} id="menu-icon" alt="" className="group-has-checked:hidden md:hidden" />
             <img src={CloseIcon} id="close-icon" alt="" className="hidden group-has-checked:block" />
--- src/components/molecules/NavBar.tsx
+++ src/components/molecules/NavBar.tsx
@@ -1,13 +1,29 @@
 import { Button } from "../atoms/Button"
+import { HamburguerLabel } from "../atoms/HamburguerLabel"
 import { Link } from "../atoms/Link"
 
 export function NavBar() {
     return (
-        <nav className="flex gap-[2.8rem] items-center">
-            <Link text="Features" link="#" />
-            <Link text="Pricing" link="#" />
-            <Link text="contact" link="#" />
-            <Button text="Login" color="red" />
-        </nav>
+        <nav className="
+        group z-1 justify-between
+        has-checked:fixed has-checked:inset-0 has-checked:bg-blue-menu
+        has-checked:flex-wrap has-checked:items-start has-checked:py-3
+        has-checked:px-[10vw]
+        ">
+
+            <HamburguerLabel />
+
+            <ul className="tracking-[1.5px] hidden leading-none gap-3
+        md:flex md:items-center
+        group-has-checked:block
+        group-has-checked:w-full
+        group-has-checked:text-white
+        group-has-checked:my-2">
+                <li><Link text="Features" link="#" /></li>
+                <li><Link text="Pricing" link="#" /></li>
+                <li><Link text="contact" link="#" /></li>
+                <li><Button text="Login" color="red" /></li>
+            </ul>
+        </nav >
     )
 }
