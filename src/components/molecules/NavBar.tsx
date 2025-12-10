281c64e10a1a341fd892a7e0c3103fa26a99d94c
Helene T <helene.teychene@gmail.com>
2025-12-10 12:44:07 +0100

    feat/created navbat component

---
+++ src/components/molecules/NavBar.tsx
@@ -0,0 +1,13 @@
+import { Button } from "../atoms/Button"
+import { Link } from "../atoms/Link"
+
+export function NavBar() {
+    return (
+        <nav className="flex gap-3 items-center bg-red">
+            <Link text="Features" link="#" />
+            <Link text="Pricing" link="#" />
+            <Link text="contact" link="#" />
+            <Button text="Login" color="red" />
+        </nav>
+    )
+}
