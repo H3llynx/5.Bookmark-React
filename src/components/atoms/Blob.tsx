5333fcf6965f4912d6973532428f8554e2582060
Helene T <helene.teychene@gmail.com>
2025-12-11 10:26:59 +0100

    feat/created blob (background blue image) component

---
+++ src/components/atoms/Blob.tsx
@@ -0,0 +1,10 @@
+export function Blob({ location }: { location: "hero" | "features" }) {
+    const variant = {
+        hero: "blob right-0 rounded-l-[12rem] top-[17%] md:top-[35%] md:w-[38%]",
+        features: "blob left-0 rounded-r-[12rem] top-[8%] md:top-[20%] md:w-[50%] md:aspect-[15/8]"
+    }
+    return (
+        <div className={variant[location]}>
+        </div>
+    )
+}
