26fb060cc77ce8ddb1a082153c8b182518abb131
Helene T <helene.teychene@gmail.com>
2025-12-12 16:25:38 +0100

    feat/created text input component

---
+++ src/components/atoms/TextInput.tsx
@@ -0,0 +1,7 @@
+export function TextInput() {
+
+    return (
+        <input className="bg-white text-[0.9rem] rounded-md py-[0.9rem] px-[1.6rem] w-[80vw] sm:w-[300px]" type="email"
+            placeholder="Enter your email address" required></input>
+    )
+}
