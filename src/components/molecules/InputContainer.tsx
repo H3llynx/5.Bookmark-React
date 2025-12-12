d1970c28db75e6b13e74e54054b3dee1a6484cde
Helene T <helene.teychene@gmail.com>
2025-12-12 16:26:26 +0100

    feat/created input container componant (includes input and error message/icon)

---
+++ src/components/molecules/InputContainer.tsx
@@ -0,0 +1,18 @@
+import ErrorIcon from "../../assets/images/icon-error.svg"
+import { TextInput } from "../atoms/TextInput"
+
+export function InputContainer() {
+
+    return (
+        <div
+            className="group relative rounded-md has-user-invalid:bg-red has-user-invalid:border-2 has-user-invalid:border-red">
+            <TextInput />
+            <img src={ErrorIcon}
+                className="hidden absolute right-[10px] top-[15px] group-has-user-invalid:block" />
+            <p
+                className="hidden text-white text-[0.7rem] text-italic text-left py-[0.2rem] px-0.5 group-has-user-invalid:block">
+                Whoops, make sure it's an
+                email</p>
+        </div>
+    )
+}
