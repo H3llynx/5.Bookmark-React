2be9d555ad2b1a8050a317677a7b4a8a2fd1e3c5
Helene T <helene.teychene@gmail.com>
2025-12-12 16:41:23 +0100

    feat/created Form component - added form button variant to the Button component

---
+++ src/components/molecules/Form.tsx
@@ -0,0 +1,12 @@
+import { Button } from "../atoms/Button";
+import { InputContainer } from "./InputContainer";
+
+export function Form() {
+
+    return (
+        <form className="flex gap-1 items-start justify-center max-w-[80vw] m-auto flex-col sm:flex-row">
+            <InputContainer />
+            <Button color="redNews" text="Contact us" />
+        </form>
+    )
+}
