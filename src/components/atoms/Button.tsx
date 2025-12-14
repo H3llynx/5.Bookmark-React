9a9d4ec102590a0c870dcf9f36f8f8630afce4d2
Helene T <helene.teychene@gmail.com>
2025-12-14 10:54:41 +0100

    fix/fixed button styling issue

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -7,7 +7,7 @@ export function Button({ text, color }: ButtonProps) {
 
   const variants = {
     redNav: "my-button uppercase py-[0.9rem] px-[0.8rem] border-white md:w-auto md:px-2 md:py-[0.7rem] md:bg-red md:text-white md:border-red md:hover:bg-white md:hover:text-red w-full group-has-checked:mt-2",
-    redNews: "my-button font-medium py-[0.9rem] px-[0.8rem] border-red bg-red text-sm text-white md:px-[1.6rem] md:py-[0.75rem] md:w-auto md:hover:bg-white md:hover:text-red w-full;",
+    redNews: "my-button font-medium py-[0.9rem] px-[0.8rem] border-red bg-red text-sm text-white md:px-[1.6rem] md:py-[0.75rem] md:w-auto md:hover:bg-white md:hover:text-red w-full",
     blueHero: "my-button blue-button py-[0.9rem] px-[0.8rem] md:px-[1.6rem]",
     blueSections: "my-button blue-button py-[0.8rem] px-[1.5rem]",
     grey: "my-button font-medium py-[0.8rem] px-[0.8rem] border-grey-btn bg-grey-btn text-grey-dark md:px-[1.6rem] hover:bg-white hover:border-grey-dark"
