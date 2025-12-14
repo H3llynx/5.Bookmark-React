8edb6094a5e8e1ba4edea389b1506fc13e762769
Helene T <helene.teychene@gmail.com>
2025-12-14 13:20:06 +0100

    refactor: added interface for typing to the FeatureTab component and improved styling

--- src/components/molecules/FeatureTab.tsx
+++ src/components/molecules/FeatureTab.tsx
@@ -1,26 +1,32 @@
 
-import FeatureImg1 from "../../assets/images/illustration-features-tab-1|2|3.svg"
+import FeatureImg1 from "../../assets/images/illustration-features-tab-1.svg"
 import FeatureImg2 from "../../assets/images/illustration-features-tab-2.svg"
 import FeatureImg3 from "../../assets/images/illustration-features-tab-3.svg"
 import { Button } from "../atoms/Button"
 import { SectionHeading } from "../atoms/SectionHeading"
 
-export function FeatureTab({ title, text, tab }: { title: string, text: string, tab: 1 | 2 | 3 }) {
+interface FeatureTabProps {
+    title: string,
+    text: string,
+    tab: "tab1" | "tab2" | "tab3"
+}
+
+export function FeatureTab({ title, text, tab }: FeatureTabProps) {
     const variant = {
-        1: FeatureImg1,
-        2: FeatureImg2,
-        3: FeatureImg3
+        tab1: FeatureImg1,
+        tab2: FeatureImg2,
+        tab3: FeatureImg3
     }
     return (
-        <article className="relative hidden w-full flex-col-reverse md:flex-row-reverse gap-3 md:gap-2">
-            <div className="w-[80vw] m-auto md:w-[45%] md:pr-[10vw] md:pl-[3vw] animate-tab-content">
+        <article className="relative md:text-left w-full flex flex-col-reverse md:flex-row-reverse gap-3 md:gap-2">
+            <div className="w-[80vw] m-auto md:w-[45%] md:pr-[10vw] md:pl-[3vw]">
                 <SectionHeading color="dark" text={title} />
-                <p className="md:text-left">{text}</p>
-                <Button color="blueSections" text="More.info" />
+                <p className="mb-2">{text}</p>
+                <Button color="blueSections" text="More info" />
             </div>
             <div className="w-[80vw] m-auto md:w-[55%] md:pl-[10vw]">
                 <img loading="lazy"
-                    className="w-full object-contain aspect-7/5 object-top md:object-top-right animate-tab-content"
+                    className="w-full object-contain aspect-7/5 object-top md:object-top-right"
                     src={variant[tab]} />
             </div>
         </article>
