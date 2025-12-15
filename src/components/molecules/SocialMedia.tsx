5f47f9eb39386f49c97d3b1122cab7488943b61c
Helene T <helene.teychene@gmail.com>
2025-12-15 21:55:08 +0100

    refactor / moved style variant outside the function

--- src/components/molecules/SocialMedia.tsx
+++ src/components/molecules/SocialMedia.tsx
@@ -1,12 +1,12 @@
 import { FacebookLink } from "../atoms/social-media/FacebookLink";
 import { TwitterLink } from "../atoms/social-media/TwitterLink";
 
-export function SocialMedia({ location }: { location: "top" | "bottom" }) {
+const variant = {
+    top: "hidden justify-center text-white gap-2.5 mt-auto mb-O group-has-checked:flex",
+    bottom: "flex gap-2.5 mt-1 mx-auto md:m-0"
+}
 
-    const variant = {
-        top: "hidden justify-center text-white gap-2.5 mt-auto mb-O group-has-checked:flex",
-        bottom: "flex gap-2.5 mt-1 mx-auto md:m-0"
-    }
+export function SocialMedia({ location }: { location: "top" | "bottom" }) {
     return (
         <div className={variant[location]}>
             <FacebookLink link="#" />
